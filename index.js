2f3e03e631163e92b5d80154bced2e516bada7fd
xsgokce <serhatgokce98@gmail.com>
2021-03-28 19:32:46 +0300

    a4

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ io.on("connection", (socket) => {
     console.log("yazmıyor", msg);
     const roomID = msg["roomID"];
     const senderName = msg["senderName"];
-    socket.broadcast.to(roomID).emit("untyping", msg);
+    io.to(roomID).emit("untyping", msg);
   });
   socket.on("typing", (msg) => {
     console.log("yazıyor", msg);
     const roomID = msg["roomID"];
     const senderName = msg["senderName"];
-    socket.broadcast.to(roomID).emit("typing", msg);
+    io.to(roomID).emit("typing", msg);
     console.log(io.sockets.adapter.rooms);
   });
   socket.on("join_room", (msg) => {
