a831701a99136d474b3f5128964a9942dbe6320a
xsgokce <serhatgokce98@gmail.com>
2021-03-28 16:50:58 +0300

    a

---
+++ app.js
@@ -0,0 +1,36 @@
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+io.on("connection", (socket) => {
+  socket.on("untyping", (msg) => {
+    console.log("yazmıyor", msg);
+    const roomID = msg["roomID"];
+    const senderName = msg["senderName"];
+    socket.broadcast.to(roomID).emit("untyping", msg);
+  });
+  socket.on("typing", (msg) => {
+    console.log("yazıyor", msg);
+    const roomID = msg["roomID"];
+    const senderName = msg["senderName"];
+    socket.broadcast.to(roomID).emit("typing", msg);
+    console.log(io.sockets.adapter.rooms);
+  });
+  socket.on("join_room", (msg) => {
+    socket.join(msg["roomID"]);
+  });
+
+  socket.on("disconnect", (reason) => {
+    console.log("disconnect");
+  });
+});
+
+if (module === require.main) {
+  const PORT = process.env.PORT || 3000;
+  server.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log("Press Ctrl+C to quit.");
+  });
+}
+
+module.exports = server;
